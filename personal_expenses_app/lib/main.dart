3ed87123a79af01b28fce7d03089429ee377bfde
Amr Hesham <ahesham95@victorylink.com>
2021-03-03 09:38:25 +0200

    - creating transaction model - model transactions list to cards

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyHomePage());
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      title: "T1",
+      amount: 22.5,
+      id: "0",
+      date: DateTime.now(),
+    ),
+    Transaction(
+      title: "T2",
+      amount: 54.2,
+      id: "1",
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("test"),
-        ),
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text("CHART"),
-                color: Colors.blue,
-              ),
-            ),
-            Card(
-              child: Text("List of TX"),
-            ),
-          ],
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
       ),
-    );
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: Card(
+              child: Text("CHART"),
+              color: Colors.blue,
+            ),
+          ),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList()),
+        ],
+      ),
+    ));
   }
 }
