efbde33a0b78526d40c03739fc764b1dd00ecc83
Amr Hesham <amrhesham9595@gmail.com>
2021-03-03 07:30:16 +0200

    no message

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyHomePage());
+}
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("test"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text("CHART"),
+                color: Colors.blue,
+              ),
+            ),
+            Card(
+              child: Text("List of TX"),
+            ),
+          ],
+        ),
       ),
-      body: Center(),
     );
   }
 }
