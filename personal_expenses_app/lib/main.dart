714e64652f6be59ffcbd7b35d2f26b403af8d7ae
Amr Hesham <ahesham95@victorylink.com>
2021-03-10 21:12:22 +0200

    implementing modal sheet

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
-import './widgets/transaction_section.dart';
+import 'package:personal_expenses_app/widgets/transaction_input.dart';
+import 'package:personal_expenses_app/widgets/transactions_list.dart';
+import 'models/transaction.dart';
 
 void main() {
-  runApp(MyHomePage());
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactionsList = [
+    Transaction(
+      title: "T1",
+      amount: 22.5,
+      id: "0",
+      date: DateTime.now(),
+    ),
+    Transaction(
+      title: "T2",
+      amount: 54.2,
+      id: "1",
+      date: DateTime.now(),
+    )
+  ];
+
+  void addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+
+    setState(() {
+      transactionsList.add(newTx);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return TransactionInput(
+              handler: (title, amount) => {addNewTransaction(title, amount)});
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +62,7 @@ class MyHomePage extends StatelessWidget {
             icon: Icon(
               Icons.add,
             ),
-            onPressed: () => {},
+            onPressed: () => startAddNewTransaction(context),
           ),
         ],
       ),
@@ -32,12 +77,15 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
-            TransactionSection(),
+            TransactionsList(
+              transactions: transactionsList,
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () => {},
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     ));
